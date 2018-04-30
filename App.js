8c6dba1ae0ce1004c299e10dc26d9485a6f16b00
Md Wasim Akhtar <v-mdakht@microsoft.com>
2018-04-30 19:45:34 +0530

    Update with new changes

--- App.js
+++ App.js
@@ -1,58 +1,91 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
 
 import React, { Component } from 'react';
 import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableOpacity,
+  AppRegistry
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import codePush from 'react-native-code-push';
 
-type Props = {};
-export default class App extends Component<Props> {
+let codePushOptions = {
+  checkFrequency: codePush.CheckFrequency.MANUAL
+
+};
+
+ class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.updateDevEnv = this.updateDevEnv.bind(this);
+    this.state = { logs: [] };
+  }
+   updateDevEnv() {  
+     this.setState({logs : ['Started at ' + new Date().getDate()]});     
+    codePush.sync({      
+      updateDialog: true,
+      installMode: codePush.InstallMode.IMMEDIATE,
+      deploymentKey: "qrtRa7u-nORN_VQAyZtUGAk8rP6PB17U2546z"
+    }, (status) => {
+      for(var key in codePush.SyncStatus){
+        if(status === codePush.SyncStatus[key]){
+          this.setState(prevState => ({ logs: [...prevState.logs, key.replace(/_/g, ' ')]}));
+        }
+      }
+    });
+  }
+
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
+      <Text style={styles.codePush}>Demo - Code Push Updated!!New</Text>
+        <TouchableOpacity onPress={ () => this.updateDevEnv() }
+          style={styles.button}>
+          <Text> Connect To Dev </Text>
+        </TouchableOpacity >
+        <View>
+        <TouchableOpacity
+          style={styles.button}>
+          <Text> Connect To SIT </Text>
+        </TouchableOpacity>
+        </View>
+        <View>
+        <TouchableOpacity
+          style={styles.button}>
+          <Text> Connect To UAT </Text>
+        </TouchableOpacity>
+        </View>
+        <View>
+          {this.state.logs.map((log, i)=> <Text> {log} </Text>)}
+        </View>
+       </View>
+     )
+   }
   }
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      paddingHorizontal: 10,
+      margin: 10
+    },
+    button: {
+      alignItems: 'center',
+      backgroundColor: '#DDDDDD',
+      padding: 10,
+      margin: 5
+    },
+    codePush:
+    {
+      fontSize: 25,
+      textAlign: 'center',
+      margin: 10
+    }
+  })
+  App = codePush(codePushOptions)(App);
+  export default App;
